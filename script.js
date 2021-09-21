a79112a562c6c25e3a43f67827ca6ff50361e4e8
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2021-09-21 16:41:03 +0200

    forEach metod added

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const list = document.querySelector('ul');
+const listElements = document.querySelectorAll('li');
 const btn = document.querySelector('button');
 
+let fontSize = 16;
+
 showList = () => {
-        list.style.display = "none" ? list.style.display = "block" : "już jest gotowe";
+       list.style.display = "none" ? list.style.display = "block" : "";
+       
+       
+  
+
+    //    for (let i = 0; i < listElements.length; i++) {
+     
+    //         listElements[i].style.fontSize = fontSize + "px";
+    //    }
+
+    listElements.forEach(item => item.style.fontSize = fontSize + "px")
+       fontSize++;
+      
 }
 
 btn.addEventListener("click", showList);
